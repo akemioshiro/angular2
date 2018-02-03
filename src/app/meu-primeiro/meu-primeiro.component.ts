b3232a67b8b3046289bbea4b17cdf783914a9c23
patricia.oshiro@totvs.com.br <patricia.oshiro@totvs.com.br>
2018-02-03 20:01:47 -0200

    criação de componentes em angular

---
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,12 @@
+import{Component} from '@angular/core';
+
+// passando metadados
+@Component({
+    selector:'meu-primeiro-component', // nome da tag html que utilizaremos
+    template: `
+        <p>Meu primeiro component criado com Angular</p>
+    `
+})
+export class MeuPrimeiroComponent{
+
+}
